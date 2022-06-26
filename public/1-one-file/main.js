172553a5fbeb2fed4f599a8e925f01d0a8c351d8
nal <nakata.kzhr@gmail.com>
2022-06-27 01:34:12 +0900

    wip

---
+++ public/1-one-file/main.js
@@ -0,0 +1,15 @@
+function main(){
+  draw()
+}
+
+/** 円を描く */
+function draw(){
+  const canvas = document.querySelector("#canvas")
+  const ctx = canvas.getContext('2d')
+
+  ctx.beginPath()
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true)
+  ctx.stroke()
+}
+
+main()
---
+++ public/3-order-read/main.js
@@ -0,0 +1,5 @@
+function main(){
+  draw()
+}
+
+main()
